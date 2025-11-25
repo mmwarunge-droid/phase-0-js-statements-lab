362e9a4cfe3e046f034a445f58106e744dcec2d6
Mark_Warunge <mmwarunge@gmail.com>
2025-11-25 08:16:49 +0300

    Fix variable redeclaration and declare missing action variable

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,25 @@
 const temperature = 90;
 const timeOfDay = "morning";
-let soilMoisture = 30;
+let soilMoisture = 10;
+let action;
+
+if (temperature >=80){
+    action = "Watering on";
+}else{
+    action = "Watering off";
+}
+console.log(action);
+
+if (timeOfDay === "evening"|| timeOfDay === "night"){
+    action = "Lights on";
+}else{
+    action = "Lights off";
+}
+console.log(action);
+
+while (soilMoisture < 40) {
+    console.log(soilMoisture);
+    soilMoisture += 5;
+}
+
+console.log(40);
